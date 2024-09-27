df876b2f1aefebe19b6547b960fa99a34a056d4a
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-09-27 13:45:51 -0600

    Fixes inlined build cache in built images, minor touchups to run_stack.sh

--- run_stack.sh
+++ run_stack.sh
@@ -1,64 +1,26 @@
 #!/usr/bin/env bash
 
-# NOTES:
-# -------
-# This script launches the molevolvr stack in the specified target environment.
-# It's invoked as ./run_stack [target_env] [docker-compose args]; if
-# [target_env] is not specified, it will attempt to infer it from the repo's
-# directory name, and aborts with an error message if it doesn't find a match.
-# the remainder of the arguments are passed along to docker compose.
-#
-# for example, to launch the stack in the "prod" environment with the "up -d"
-# command, you would run: ./run_stack prod up -d
-#
-# the available environments differ in a variety of ways, including:
-# - which services they run (prod runs 'nginx', for example, but the dev-y envs
-#   don't)
-# - cores and memory constraints that are applied to the SLURM containers, in
-#   environments where the job scheduler is enabled
-# - what external resources they mount as volumes into the container; for
-#   example, each environment mounts a different job results folder, but
-#   environments that process jobs use the same blast and iprscan folders, since
-#   they're gigantic
-#
-# these differences between environments are implemented by invoking different
-# sets of docker-compose.yml files. with the exception of the "app" environment,
-# the "root" compose file, docker-compose.yml, is always used first, and then
-# depending on the environment other compose files are added in, which merge
-# with the root compose configuration. since the app environment only runs the
-# app, it has a separate compose file, docker-compose.apponly.yml, rather than
-# merging with the root and killing nearly all the services except the app
-# service.
-#
-# see the following for details on the semantics of merging compose files:
-# https://docs.docker.com/compose/multiple-compose-files/merge/
-#
-# the current environments are as follows (contact FSA for details):
-# - prod: the production environment, which runs the full stack, including the
-#   web app, the job scheduler, and the accounting database. it's the most
-#   resource-intensive environment, and is intended for use in production.
-# - dev/staging: these are effectively dev environments that specific users run
-#   on the server for testing purposes.
-# - app: a development environment that runs only the frontend and backend, and
-#   not the job scheduler or the accounting database. it's intended for use in
-#   frontend development, where you don't need to submit jobs or query the
-#   accounting database.
 
+# ===========================================================================
+# === user-overiddeable variables
+# ===========================================================================
 
-# if 1, skips invoking ./build_images.sh before running the stack
+# if 1, skips pulling images for the target environment before running
+SKIP_PULL=${SKIP_PULL:-0}
+
+# if 1, skips building images for the target environment before running
 SKIP_BUILD=${SKIP_BUILD:-0}
 
-# command to run after the stack has launched, e.g.
-# in cases where you want to tail some containers after launch
-# (by default, it does nothing)
-POST_LAUNCH_CMD=":"
-# if 1, clears the screen before running the post-launch command
-DO_CLEAR="0"
 # if 1, opens the browser window to the app after launching the stack
 DO_OPEN_BROWSER=${DO_OPEN_BROWSER:-1}
 
 # the URL to open when we invoke the browser
-FRONTEND_URL=${FRONTEND_URL:-"http://localhost:5713"}
+FRONTEND_URL=${FRONTEND_URL:-"http://localhost:5173"}
+
+
+# ===========================================================================
+# === helpers
+# ===========================================================================
 
 # helper function to print a message and exit with a specific code
 # in one command
@@ -80,9 +42,15 @@ function open_browser() {
     fi
 }
 
+
 # ===========================================================================
-# === entrypoint
+# === target env resolution and other env/cmd setup for launching the stack
 # ===========================================================================
+
+# if 1, clears the screen before running the post-launch command
+DO_CLEAR="0"
+# command to run after the stack has launched
+POST_LAUNCH_CMD=":"
 
 # source the .env file and export its contents
 # among other things, we'll use the DEFAULT_ENV var in it to set the target env
@@ -91,22 +59,17 @@ source .env
 set +a
 
 # check if the first argument is a valid target env, and if not attempt
-# to infer it from the script's parents' directory name
+# to infer it
 case $1 in
-    "prod"|"staging"|"dev"|"app")
+    "prod"|"dev")
         TARGET_ENV=$1
         shift
         echo "* Selected target environment: ${TARGET_ENV}"
         ;;
     *)
-        # attempt to resolve the target env from the host environment
-        # (e.g., the hostname, possibly the repo root directory name, etc.)
-
-        # get the name of the script's parent directory
-        PARENT_DIR=$(basename $(dirname $(realpath $0)))
+        # resolve the target env
         HOSTNAME=$(hostname)
-
-        # check if the parent directory name contains a valid target env
+        
         if [[ "${HOSTNAME}" = "jravilab" ]]; then
             TARGET_ENV="prod"
             STRATEGY="via hostname ${HOSTNAME}"
@@ -145,13 +108,23 @@ case ${TARGET_ENV} in
         exit 1
 esac
 
-# ensure that docker compose can see the target env, so it can, e.g., namespace hosts to their environment
+
+# vars that configure (and thus need to be visible to) docker-compose and other
+# child processes
 export TARGET_ENV=${TARGET_ENV}
-# ensure that the registry prefix is exported to the env as well
 export REGISTRY_PREFIX=${REGISTRY_PREFIX}
-# for tags used when pulling/building images, use 'latest' if we're running prod, and use TARGET_ENV otherwise
-# FIXME: we should consider a version-based tagging system
 export IMAGE_TAG=$( [[ ${TARGET_ENV} = "prod" ]] && echo "latest" || echo ${TARGET_ENV} )
+
+# image build controls:
+# makes compose use the regular docker cli build command
+export COMPOSE_DOCKER_CLI_BUILD=1
+# makes docker use buildkit rather than the legacy build system
+export DOCKER_BUILDKIT=1
+
+
+# ===========================================================================
+# === final argument processing, stack launch
+# ===========================================================================
 
 # if any arguments were specified after the target env, use those instead of the default
 if [ $# -gt 0 ]; then
@@ -174,13 +147,20 @@ if [[ "$1" =~ ^(down|restart|logs|build|pull|push|shell)$ ]]; then
     fi
 fi
 
-# if SKIP_BUILD is 0 and 'down' isn't the docker compose command, build images
-# for the target env.
+# before building, attempt to pull the images for the target env
+# the images now include the build cache, which should make rebuilding
+# the final layers quick.
+if [ "${SKIP_PULL}" != "1" ]; then
+    echo "* Pulling images for ${TARGET_ENV} (tag: ${IMAGE_TAG})"
+    ${COMPOSE_CMD} pull || fatal "Failed to pull images for ${TARGET_ENV}"
+fi
+
+# if SKIP_BUILD is 0 and we're not running a control command, build images for
+# the target env.
 # each built image is tagged with its target env, so they don't collide with
 # each other; in the case of prod, the tag is "latest".
-if [ "${SKIP_BUILD}" -eq 0 ]; then
+if [ "${SKIP_BUILD}" != "1" ]; then
     echo "* Building images for ${TARGET_ENV} (tag: ${IMAGE_TAG})"
-    # ./build_images.sh ${IMAGE_TAG} || fatal "Failed to build images for ${TARGET_ENV}"
     ${COMPOSE_CMD} build || fatal "Failed to build images for ${TARGET_ENV}"
 fi
 
@@ -191,5 +171,5 @@ ${COMPOSE_CMD} ${DEFAULT_ARGS} && \
     [[ ${DO_OPEN_BROWSER} = "1" ]] \
         && open_browser "${FRONTEND_URL}" \
         || exit 0
-) &&
+) && \
 ${POST_LAUNCH_CMD}
