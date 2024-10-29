1d3bf0b05ba77f44617f7d35fbc86acc4ca3b3f5
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-10-29 17:56:40 -0400

    reformat playwright config

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -8,16 +8,10 @@ export default defineConfig({
   fullyParallel: true,
   workers: "75%",
   reporter: "html",
-  use: {
-    baseURL: url,
-    // headless: false,
-  },
+  use: { baseURL: url },
 
   projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
     { name: "webkit", use: { ...devices["Desktop Safari"] } },
     { name: "firefox", use: { ...devices["Desktop Firefox"] } },
   ],
