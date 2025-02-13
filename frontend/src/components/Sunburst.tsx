4150674b8465dd4f5bc91c1f3d5225ea5faf5f89
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-02-12 21:57:10 -0500

    incorporate more d3 util funcs

--- frontend/src/components/Sunburst.tsx
+++ frontend/src/components/Sunburst.tsx
@@ -1,5 +1,5 @@
 import {
-  useCallback,
+  Fragment,
   useEffect,
   useId,
   useMemo,
@@ -14,7 +14,7 @@ import {
   FaRegImage,
 } from "react-icons/fa6";
 import clsx from "clsx";
-import { arc } from "d3";
+import { arc, hierarchy, type HierarchyNode } from "d3";
 import { inRange, startCase, sumBy, truncate } from "lodash";
 import { useDebounce, useElementSize } from "@reactuses/core";
 import Button from "@/components/Button";
@@ -25,7 +25,6 @@ import { useColorMap } from "@/util/color";
 import { fitViewBox, printElement } from "@/util/dom";
 import { downloadJpg, downloadPng, downloadSvg } from "@/util/download";
 import { useTheme } from "@/util/hooks";
-import { flatMap } from "@/util/object";
 import { formatNumber } from "@/util/string";
 import classes from "./Sunburst.module.css";
 
@@ -42,6 +41,27 @@ export type Item = {
   children?: Item[];
 };
 
+type Derived = {
+  label: string;
+  type: string;
+  value: number;
+  children: Derived[];
+  /** color mapped from type */
+  color: string;
+  /** percent of full circle that item takes up, from 0 to 1 */
+  percent: number;
+  /** start angle of item, from 0 to 1 percent */
+  angle: number;
+  /** is item part of selected breadcrumbs */
+  selected: boolean | null;
+  /** is item last of selected breadcrumbs */
+  lastSelected: boolean | null;
+  /** temporary value to track children's angles */
+  childAngle: number;
+};
+
+type Node = HierarchyNode<Derived>;
+
 type Props = {
   /** chart title */
   title?: string;
@@ -49,27 +69,12 @@ type Props = {
   data: Item[];
 };
 
-type ItemDerived = {
-  label: string;
-  value: number;
-  /** value, normalized to % of parent value */
-  percent: number;
-  type: string;
-  color: string;
-  selected: boolean | null;
-  lastSelected: boolean | null;
-  indexPath: number[];
-  children: ItemDerived[];
-};
-
-/** options, in svg units (relative): */
-
-/** thickness of rings */
+/** thickness of rings, in svg units */
 const ringSize = 20;
-/** "level" (multiple of ring size) of first ring from center */
-const startLevel = 3;
-/** gap between rings */
+/** gap between rings, in svg units */
 const gapSize = 2;
+/** depth/level of first ring from center */
+const startDepth = 1;
 
 const Sunburst = ({ title, data }: Props) => {
   const container = useRef<HTMLDivElement>(null);
@@ -91,116 +96,81 @@ const Sunburst = ({ title, data }: Props) => {
     setFontSize(viewBox.height * (docFontSize / clientHeight));
   }, [clientHeight]);
 
-  /** "trail" of breadcrumbs through tree of items */
-  const [breadcrumbs, setBreadcrumbs] = useState<ItemDerived[]>([]);
+  /** "breadcrumb trail" of selected nodes */
+  const [selected, setSelected] = useState<Node[]>([]);
 
-  /** datum types */
-  const types = flatMap(data, "type").filter(Boolean) as string[];
+  /** are any nodes selected */
+  const anySelected = !!selected.length;
 
-  /** map datum type to color */
+  /** hierarchical data structure with convenient access methods */
+  const tree = useMemo(() => {
+    const tree = hierarchy<Derived>({ children: data } as Derived);
+
+    /** set fallbacks */
+    for (const { data } of tree) {
+      data.label ??= "";
+      data.type ??= "-";
+      data.value ??= 0;
+      data.color ??= "";
+      data.percent ??= 1;
+      data.angle ??= 0;
+      data.childAngle ??= 0;
+    }
+
+    /** go down tree recursively */
+    tree.eachBefore(({ data, parent }) => {
+      if (!parent) return;
+      /** get total of siblings' values */
+      const total = sumBy(parent?.children, (d) => d.data.value);
+      /** get this node's value as percent of total */
+      const percent = data.value / total || 0;
+      /** get percent of relative to parent, i.e. percent of full circle */
+      data.percent = parent.data.percent * percent;
+      /** set angle from parent's current child angle */
+      data.angle = parent.data.childAngle;
+      data.childAngle = parent.data.childAngle;
+      /** increment parent's child angle */
+      parent.data.childAngle += data.percent;
+    });
+
+    return tree;
+  }, [data]);
+
+  /** get all nodes' types */
+  const types = useMemo(() => {
+    const types: string[] = [];
+    tree.each((node) => types.push(node.data.type));
+    return types;
+  }, [tree]);
+
+  /** map of node type to color */
   const colorMap = useColorMap(types, "mode");
 
-  /** derive props */
-  const derive = useCallback(
-    (items: Item[]) => {
-      const derive = (
-        items: Item[],
-        parentPercent = 1,
-        parentIndexPath: ItemDerived["indexPath"] = [],
-      ) => {
-        const newItems: ItemDerived[] = [];
+  /** convert node tree to list and derive some more props */
+  const nodes = useMemo(
+    () =>
+      [...tree].map((node) => {
+        /** assign color from type */
+        node.data.color = colorMap[node.data.type]!;
 
-        /** total of children's values */
-        const total = sumBy(items, "value");
+        /** selected state */
+        node.data.selected = anySelected ? selected.includes(node) : null;
+        node.data.lastSelected = anySelected ? selected.at(-1) === node : null;
 
-        /** for each (child) item */
-        for (let index = 0; index < items.length; index++) {
-          const {
-            type = "",
-            label = "-",
-            value,
-            children = [],
-          } = items[index]!;
-
-          /** normalize value to percent of full circle that it takes up */
-          const percent = parentPercent * (value / total) || 0;
-
-          /** path of indices to get to this item through tree */
-          const indexPath = [...parentIndexPath, index];
-
-          /** are any items selected */
-          const anySelected = !!breadcrumbs.length;
-
-          /** is this item selected */
-          const selected = !!breadcrumbs
-            .at(-1)
-            ?.indexPath?.join("-")
-            ?.startsWith(indexPath.join("-"));
-
-          /** is this item outer-most selected */
-          const lastSelected =
-            breadcrumbs.at(-1)?.indexPath?.join("-") === indexPath?.join("-");
-
-          /** item color */
-          const color = colorMap[type]!;
-
-          /** data point values and styles */
-          newItems.push({
-            label,
-            value,
-            percent,
-            type,
-            color,
-            selected: anySelected ? selected : null,
-            lastSelected: anySelected ? lastSelected : null,
-            indexPath,
-
-            /** do same thing for child items recursively */
-            children: derive(children, percent, indexPath),
-          });
-        }
-        return newItems;
-      };
-
-      return derive(items);
-    },
-    [colorMap, breadcrumbs],
-  );
-
-  /** derive data */
-  const derived = derive(data);
-
-  /** select item */
-  const selectItem = useCallback<SegmentProps["selectItem"]>(
-    ({ indexPath, lastSelected }: Partial<ItemDerived>) => {
-      /** is outer-most selected */
-      if (lastSelected) {
-        /** de-select */
-        setBreadcrumbs([]);
-      } else {
-        /** select */
-        const breadcrumbs: ItemDerived[] = [];
-        /** start at root */
-        let children = derived;
-        /** get list of items from indices */
-        for (const index of indexPath ?? []) {
-          const breadcrumb = children[index]!;
-          breadcrumbs.push(breadcrumb);
-          children = breadcrumb.children;
-        }
-        setBreadcrumbs(breadcrumbs);
-      }
-    },
-    [derived],
+        return node;
+      }),
+    [tree, colorMap, selected, anySelected],
   );
 
   return (
     <Flex direction="column" gap="lg" full>
+      {/* keyboard listener not necessary here because we have one below */}
       {/* eslint-disable-next-line */}
       <div
         ref={container}
         className={clsx("card", classes.container)}
-        onClick={() => setBreadcrumbs([])}
+        /** deselect */
+        onClick={() => setSelected([])}
       >
         {title && <strong>{title}</strong>}
 
@@ -220,15 +190,26 @@ const Sunburst = ({ title, data }: Props) => {
 
         {/* chart container */}
         <svg ref={svg} className={classes.chart}>
-          <Segment
-            children={derived}
-            selectItem={selectItem}
-            fontSize={fontSize}
-          />
+          {nodes.map((node, index) => (
+            <Fragment key={index}>
+              {node.parent && (
+                <Segment
+                  fontSize={fontSize}
+                  select={() =>
+                    node.data.lastSelected
+                      ? setSelected([])
+                      : setSelected(node.ancestors().slice(0, -1).reverse())
+                  }
+                  deselect={() => setSelected([])}
+                  node={node}
+                />
+              )}
+            </Fragment>
+          ))}
         </svg>
 
-        {/* breadcrumbs */}
-        {!!breadcrumbs.length && (
+        {/* selected breadcrumbs */}
+        {anySelected && (
           <Flex
             className={classes.breadcrumbs}
             gap="sm"
@@ -237,17 +218,17 @@ const Sunburst = ({ title, data }: Props) => {
             hAlign="right"
             vAlign="top"
           >
-            {breadcrumbs.map((item, index) => (
-              <ItemTooltip key={index} {...item}>
+            {selected.map((node, index) => (
+              <NodeTooltip key={index} {...node}>
                 <div
                   className={classes.breadcrumb}
-                  style={{ background: item.color }}
+                  style={{ background: node.data.color }}
                   tabIndex={0}
                   role="button"
                 >
-                  {formatPercent(item.percent)} {item.label}
+                  {formatPercent(node.data.percent)} {node.data.label}
                 </div>
-              </ItemTooltip>
+              </NodeTooltip>
             ))}
           </Flex>
         )}
@@ -307,28 +288,25 @@ const Sunburst = ({ title, data }: Props) => {
 
 export default Sunburst;
 
-type SegmentProps = Partial<ItemDerived> & {
+type SegmentProps = {
   fontSize: number;
-  level?: number;
-  startAngle?: number;
-  endAngle?: number;
-  selectItem: (item: Partial<ItemDerived>) => void;
+  node: Node;
+  select: () => void;
+  deselect: () => void;
 };
 
 /** single arc segment */
-const Segment = ({
-  fontSize,
-  level = 0,
-  startAngle = 0,
-  endAngle = 1,
-  selectItem,
-  ...item
-}: SegmentProps) => {
+const Segment = ({ fontSize, node, select, deselect }: SegmentProps) => {
   /** unique segment id */
   const id = useId();
 
+  /** extract props */
+  const { depth, data } = node;
+  const { label, color, percent, angle, selected, lastSelected } = data;
+  const end = angle + percent;
+
   /** segment arc radius */
-  const radius = (level + startLevel - 0.5) * ringSize;
+  const radius = (depth + startDepth - 0.5) * ringSize;
 
   /** get enclosed shape to fill */
   const fill = useMemo(
@@ -336,29 +314,30 @@ const Segment = ({
       arc<null>()
         .innerRadius(radius - ringSize / 2 + gapSize / 2)
         .outerRadius(radius + ringSize / 2 - gapSize / 2)
-        .startAngle(startAngle * 2 * Math.PI)
-        .endAngle(endAngle * 2 * Math.PI)
+        .startAngle(angle * 2 * Math.PI)
+        .endAngle(end * 2 * Math.PI)
         .padRadius(gapSize)
         .padAngle(1)(null) ?? "",
-    [radius, startAngle, endAngle],
+    [radius, angle, end],
   );
 
   /** get stroke path */
   const stroke = useMemo(() => {
     /** if angle midpoint in lower half of circle, flip text so not upside down */
-    const flip = inRange((startAngle + endAngle) / 2, 0.25, 0.75);
+    const flip = inRange((angle + end) / 2, 0.25, 0.75);
 
     let stroke =
       arc<null>()
+        /** center line of segment */
         .innerRadius(radius)
         /**
-         * subtract arbitrary number, because we'll discard latter half of path.
-         * d3 always "draws" (in svg path command) larger radius first, so do
-         * negative.
+         * centerline, minus some thickness to ensure there is L command. d3
+         * does A command(s) for larger radius first, and we will only keep
+         * that, so thickness can be arbitrary.
          */
-        .outerRadius(radius - Infinity)
-        .startAngle((flip ? endAngle : startAngle) * 2 * Math.PI)
-        .endAngle((flip ? startAngle : endAngle) * 2 * Math.PI)
+        .outerRadius(radius - 999)
+        .startAngle((flip ? end : angle) * 2 * Math.PI)
+        .endAngle((flip ? angle : end) * 2 * Math.PI)
         .padRadius(gapSize)
         .padAngle(1)(null) ?? "";
 
@@ -366,98 +345,68 @@ const Segment = ({
     stroke = stroke.slice(0, stroke.indexOf("L"));
 
     return stroke;
-  }, [radius, startAngle, endAngle]);
+  }, [radius, angle, end]);
 
   /** get max text chars based on arc length */
-  const maxChars =
-    (radius * 2 * Math.PI * (endAngle - startAngle)) / (fontSize / 1.5);
-
-  /** track child angle */
-  let offsetAngle = startAngle;
+  const maxChars = (radius * 2 * Math.PI * percent) / (fontSize / 1.5);
 
   /** reactive CSS vars */
   const theme = useTheme();
 
   return (
-    <>
-      {/* this segment */}
-      {level > 0 && (
-        <g
-          className={classes.segment}
-          opacity={item.selected === false ? 0.35 : 1}
-        >
-          {/* shape */}
-          <ItemTooltip {...item}>
-            <path
-              className={classes.shape}
-              fill={
-                item.selected === false ? theme["--light-gray"] : item.color
-              }
-              stroke={theme["--black"]}
-              strokeWidth={gapSize / 2}
-              strokeOpacity={item.lastSelected === true ? 1 : 0}
-              d={fill}
-              tabIndex={0}
-              role="button"
-              onClick={(event) => {
-                /** prevent deselect from container onClick */
-                event.stopPropagation();
-                selectItem(item);
-              }}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  event.preventDefault();
-                  selectItem(item);
-                }
-              }}
-            />
-          </ItemTooltip>
-
-          {/* text path */}
-          <path id={id} fill="none" d={stroke} />
-          {/* text */}
-          <text
-            className={classes.label}
-            textAnchor="middle"
-            // dominantBaseline="central"
-            dy="0.55ex"
-            fontSize={fontSize}
-            fill={theme["--black"]}
-          >
-            <textPath href={`#${id}`} startOffset="50%">
-              {truncate(`${formatPercent(item.percent)} ${item.label}`, {
-                length: maxChars,
-              })}
-            </textPath>
-          </text>
-        </g>
-      )}
-
-      {/* recursive children segments */}
-      {item.children?.map((item, index) => (
-        <Segment
-          key={index}
-          {...item}
-          fontSize={fontSize}
-          level={level + 1}
-          startAngle={offsetAngle}
-          endAngle={(offsetAngle = offsetAngle + item.percent)}
-          children={item.children}
-          selectItem={selectItem}
+    <g className={classes.segment}>
+      {/* shape */}
+      <NodeTooltip {...data}>
+        <path
+          className={classes.shape}
+          fill={selected === false ? theme["--light-gray"] : color}
+          stroke={theme["--black"]}
+          strokeWidth={gapSize / 2}
+          strokeOpacity={lastSelected === true ? 1 : 0}
+          d={fill}
+          tabIndex={0}
+          role="button"
+          onClick={(event) => {
+            /** prevent deselect from container onClick */
+            event.stopPropagation();
+            select();
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              select();
+            }
+            if (event.key === "Escape") deselect();
+          }}
         />
-      ))}
-    </>
+      </NodeTooltip>
+
+      {/* text path */}
+      <path id={id} fill="none" d={stroke} />
+      {/* text */}
+      <text
+        className={classes.label}
+        textAnchor="middle"
+        dy="0.55ex"
+        fontSize={fontSize}
+        fill={theme["--black"]}
+      >
+        <textPath href={`#${id}`} startOffset="50%">
+          {truncate(`${formatPercent(percent)} ${label}`, { length: maxChars })}
+        </textPath>
+      </text>
+    </g>
   );
 };
 
-/** tooltip for data item */
-const ItemTooltip = ({
+/** tooltip for data node */
+const NodeTooltip = ({
   label,
   value,
   percent,
   type,
   children,
-}: Omit<Partial<ItemDerived>, "children"> & { children: ReactElement }) => (
+}: Omit<Partial<Derived>, "children"> & { children: ReactElement }) => (
   <Tooltip
     content={
       <div className="mini-table">
