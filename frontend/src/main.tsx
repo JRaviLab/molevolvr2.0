b60af677761c5088eb2c85def1d8f84a4406607a
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-02-06 17:05:49 -0500

    remove unneeded msw opt

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,27 +4,23 @@ import { createRoot } from "react-dom/client";
 import { url } from "@/util/url";
 import App from "./App";
 
-const { MODE, BASE_URL } = import.meta.env;
+const { MODE } = import.meta.env;
 
 console.debug({ env: import.meta.env });
 
 /** whether to mock network requests with fake responses */
 const mock = url.searchParams.get("mock") === "false" ? false : true;
 
-(async () => {
-  /** mock network/api calls */
-  if (mock || MODE === "test") {
-    const { setupWorker } = await import("msw/browser");
-    const { handlers } = await import("../fixtures");
-    await setupWorker(...handlers).start({
-      serviceWorker: { url: BASE_URL + "mockServiceWorker.js" },
-    });
-  }
+/** mock network/api calls */
+if (mock || MODE === "test") {
+  const { setupWorker } = await import("msw/browser");
+  const { handlers } = await import("../fixtures");
+  await setupWorker(...handlers).start();
+}
 
-  /** render app entrypoint */
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-})();
+/** render app entrypoint */
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
